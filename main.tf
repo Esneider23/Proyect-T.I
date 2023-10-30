3abc77ed22e3b1f4977023c8c4695f1b4e283538
Esneider23 <guzmane@utb.edu.co>
2023-10-30 07:59:02 -0500

    update

--- main.tf
+++ main.tf
@@ -30,18 +30,3 @@ resource "azurerm_resource_group" "database" {
   name     = "database"
   location = "eastus"
 }
-
-resource "azurerm_cosmosdb_postgresql_cluster" "database" {
-  name                            = "database-proyect"
-  resource_group_name             = azurerm_resource_group.database.name
-  location                        = azurerm_resource_group.database.location
-  administrator_login_password    = "${var.password}"
-  coordinator_storage_quota_in_mb = 131072
-  coordinator_vcore_count         = 2
-  node_count                      = 0
-}
-output "cosmosdb_connectionstrings" {
-   value = "host=c-${azurerm_cosmosdb_postgresql_cluster.database.name}.postgres.cosmos.azure.com port=5432;dbname=citus;user=citus;password=${azurerm_cosmosdb_postgresql_cluster.database.administrator_login_password};sslmode=require"
-   sensitive   = true
-}
-
