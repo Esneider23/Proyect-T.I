bf9a66a7e16e4bf8261c3ac47afe7d9493645739
Esneider23 <guzmane@utb.edu.co>
2023-10-30 16:30:38 -0500

    up

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ terraform {
 provider "azurerm" {
   features {
   }
+  SUBSCRIPTION_ID= "b3a844cf-108f-48bb-a2ce-33911078b034"
+  TENANT_ID= "88cf8521-ceb7-4d6a-a22f-d87438337dd3"
+  CLIENT_SECRET= "-l48Q~PbjVfLJMeL8ONQJPAVfBuFQFQG1QedtadR"
+  CLIENT_ID=     "c46dc770-b2e8-48d8-ae2a-96cac7b216ea"
+   
 }
 
 variable "imagebuild" {
@@ -27,4 +32,19 @@ variable "imagebuild" {
 resource "azurerm_resource_group" "rg_utbapp" {
   name = "rg_utbapp" # this is the name on azure
   location = "eastus" # data center location on azure
+}
+
+resource "azurerm_resource_group" "database" {
+  name = "database" 
+  location = "eastus" 
+}
+
+resource "azurem_cosmosdb_postgresql_cluster" "database"{
+    name = "database-cluster"
+    resource_group_name             = azurerm_resource_group.database.name
+    location                        = azurerm_resource_group.database.location
+    administrator_login_password    = "H@Sh1CoR3!"
+    coordinator_storage_quota_in_mb = 131072
+    coordinator_vcore_count         = 2
+    node_count                      = 0
 }
